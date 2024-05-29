25a551745be2ffb05997c071833980444a598c99
Ash Ajida <ashraf.ajida@remarkable.com>
2024-05-29 22:30:29 +0100

    more changes

--- src/app/components/PriceCard.tsx
+++ src/app/components/PriceCard.tsx
@@ -12,12 +12,15 @@ type Props = {
 const PriceCard = ({ type, price, body,  buttonLink, children }: Props) => {
   return (
     <div className={`col-span-1 flex flex-col p-6 w-full rounded-md backdrop-filter backdrop-blur-xl bg-white border border-white bg-opacity-10`}>
-      <div className="self-center text-lg font-semibold leading-8 text-white">
+      <div className="text-lg font-semibold leading-8 text-white">
         {type}
       </div>
       <div className="flex flex-col px-1.5 pt-2.5 mt-7">
-        <div className="self-center text-5xl font-bold leading-8 text-white max-md:text-4xl">
+        <div className="text-5xl font-bold leading-[48px] mb-2 text-white max-md:text-4xl">
           {price}
+        </div>
+        <div className="leading-8= text-white">
+          PER CLEANER / PER HOUR
         </div>
         <div className="mt-9 text-base leading-[22px] text-white">
           {body}
